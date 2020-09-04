03561e5e5f4ffbefb959fbae9b80d19292d9fcbe
cazabe <cazcarlos@hotmail.com>
2020-09-04 16:57:25 -0500

    Style: footer with social. Adding fonts to proyect

--- src/Components/Layout/Footer/Footer.component.js
+++ src/Components/Layout/Footer/Footer.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Row, Col } from "reactstrap";
 import logoTagExpress from "../../../Assets/logo-tag-express.png";
 import "./footer.style.css";
 
@@ -7,39 +8,54 @@ const FooterComponent = () => {
     // Footer
     <footer className="footer-distributed">
       <div className="footer-left">
-        <img
-          src={logoTagExpress}
-          alt="logo TagExpress"
-        />
+        <img src={logoTagExpress} alt="logo TagExpress" />
         <h3>
           AYUDANDO A<span> MOVILIZARTE</span>
         </h3>
       </div>
       <div className="footer-center">
-        <div style={{padding:"5px"}}>
+        <div style={{ padding: "5px" }}>
           <i className="fa fa-map-marker"></i>
-          <p>
-            Av. Joaquín Orrantia 124 y Av. Leopoldo Benítez, Edif. Trade
-            Building. Piso 3, Oficina 316.
-          </p>
-          <div style={{padding:"5px"}}>
+          <p>Autopista General Rumiñahui</p>
+          <div style={{ padding: "5px" }}>
             <i className="fa fa-phone"></i>
             <p>0981218994</p>
           </div>
-          <div style={{padding:"5px"}}>
+          <div style={{ padding: "5px" }}>
             <i className="fa fa-envelope"></i>
             <p>
               <a href="mailto:support@eduonix.com">
-                ceo@barriolindo.com.ec
+                servicioalclientecalprandina@tagexpress.club
               </a>
             </p>
           </div>
         </div>
       </div>
       <div className="footer-right">
-        <p className="footer-company-about">
-          © 2019 Barrio lindo. Todos los derechos reservados
+      <div style={{padding:"60px"}}>
+        <Row md="12">
+          <Col sm="4">
+            
+            <a href="#" className="fa fa-facebook"></a>
+           
+          </Col>
+          <Col sm="4">
+         
+          <a href="#" className="fa fa-instagram"></a>
+          
+          </Col>
+          <Col sm="4">
+         
+          <a href="#" className="fa fa-twitter"></a>
+         
+          </Col>
+          <div>
+          <p className="footer-company-about">
+          © 2019 CALPRANDINA . Todos los derechos reservados
         </p>
+        </div>
+        </Row>
+        </div>
       </div>
     </footer>
   );
