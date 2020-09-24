6eb9322760e0d9707e94d7b6235dc0100f479dc6
cazabe <cazcarlos@hotmail.com>
2020-09-23 20:00:14 -0500

    fixing responsive on laptops and smaller devices

--- src/Components/Layout/Pages/heroImage/Hero.component.js
+++ src/Components/Layout/Pages/heroImage/Hero.component.js
@@ -3,7 +3,7 @@ import "./hero.style.css";
 
 const HeroComponent = () => {
   return (
-    <div className="hero-image">
+    <div className="hero-image img-fluid">
       <div className="hero-text">
         {/* <button>Hire me</button> */}
       </div>
