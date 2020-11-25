bf4d1486a04032505a66ab206c30f6397a33608e
cazabe <cazcarlos@hotmail.com>
2020-11-25 08:54:22 -0500

    Changing black friday promotion an adding meta tag for browser search

--- src/Components/Layout/Pages/modal/modal.js
+++ src/Components/Layout/Pages/modal/modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import blackFRiday from "../../../../Assets/blackFRiday.jpeg";
+import blackFRiday from "../../../../Assets/blackfridaynew.jpeg";
 
 const ModalExample = (props) => {
   const {
